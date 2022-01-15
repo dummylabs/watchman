ba2e69c764510ba864737858a09987c8b83f6898
scratcha <scratcha@gmail.com>
2022-01-15 13:05:59 +0300

    code formatting

--- apps/watchman/utils.py
+++ apps/watchman/utils.py
@@ -1,39 +1,46 @@
+'''Miscellaneous support functions for watchman'''
 import glob
-import os
 import re
 
 def folder_included(path, excluded_folders):
+    '''Check if a folder should be included into scan'''
     for folder in excluded_folders:
         if folder in path:
             return False
     return True
 
 def get_next_file(folder_list, excluded_folders):
+    '''Returns next file for scan'''
     for folder in folder_list:
-        #root = os.path.join(folder, '**/*.yaml')
         for filename in glob.iglob(folder, recursive=True):
             if folder_included(filename, excluded_folders):
                 yield filename
 
 def add_entry(_list, entry, yaml_file, lineno):
+    '''Add entry to list of missing entities with line number information'''
     if entry in _list:
-        if yaml_file in _list[entry]: 
+        if yaml_file in _list[entry]:
             _list[entry].get(yaml_file, []).append(lineno)
     else:
         _list[entry] = {yaml_file: [lineno]}
 
 def parse(folders, excluded_folders, logger=None):
+    '''Parse a yaml or json file for entities/services'''
     files_parsed = 0
-    entity_pattern = re.compile("(?:(?<=\s)|(?<=^)|(?<=\")|(?<=\'))([A-Za-z_0-9]*\s*:)?(?:\s*)?((air_quality|alarm_control_panel|alert|automation|binary_sensor|button|calendar|camera|climate|counter|device_tracker|fan|group|humidifier|input_boolean|input_number|input_select|light|media_player|number|person|proximity|scene|script|select|sensor|sun|switch|timer|vacuum|weather|zone)\.[A-Za-z_*0-9]+)")
-    service_pattern = re.compile("service:\s*([A-Za-z_0-9]*\.[A-Za-z_0-9]+)")
-    comment_pattern = re.compile('#.*')
+    entity_pattern = re.compile(r"(?:(?<=\s)|(?<=^)|(?<=\")|(?<=\'))([A-Za-z_0-9]*\s*:)?(?:\s*)?"
+    r"((air_quality|alarm_control_panel|alert|automation|binary_sensor|button|calendar|camera|"
+    r"climate|counter|device_tracker|fan|group|humidifier|input_boolean|input_number|"
+    r"input_select|light|media_player|number|person|proximity|scene|script|select|sensor|"
+    r"sun|switch|timer|vacuum|weather|zone)\.[A-Za-z_*0-9]+)")
+    service_pattern = re.compile(r"service:\s*([A-Za-z_0-9]*\.[A-Za-z_0-9]+)")
+    comment_pattern = re.compile(r'#.*')
     entity_list = {}
     service_list = {}
     for yaml_file in get_next_file(folders, excluded_folders):
         #if logger:
         #    logger.log(f'opening {yaml_file} file', level="DEBUG")
         files_parsed += 1
-        for i, line in enumerate(open(yaml_file)):
+        for i, line in enumerate(open(yaml_file, encoding='utf-8')):
             line = re.sub(comment_pattern, '', line)
             for match in re.finditer(entity_pattern, line):
                 typ, val = match.group(1), match.group(2)
--- apps/watchman/watchman.py
+++ apps/watchman/watchman.py
@@ -1,12 +1,10 @@
 #encoding: utf-8
-import appdaemon.plugins.hass.hassapi as hass
-from datetime import datetime
-import utils
-import re
+'''Watchman AppDaemon application module'''
 import os
-import yaml
 import time
 import fnmatch
+import appdaemon.plugins.hass.hassapi as hass
+import utils
 
 APP_NAME = "watchman"
 APP_CFG_PATH = "/config/appdaemon/apps/watchman/watchman.yaml"
@@ -14,25 +12,27 @@ EVENT_NAME = "ad.watchman.audit"
 APP_FOLDER = "/config/appdaemon/apps/watchman"
 
 class Watchman(hass.Hass):
+    '''Watchman AppDaemon application'''
     def initialize(self):
+        '''Runs when application (re-)started'''
         self.check_lovelace = self.args.get('lovelace_ui', False)
         self.debug_log = self.args.get('debug_log', False)
         self.included_folders = self.args.get('included_folders', ['/config'])
 
         if not isinstance(self.included_folders, list) or len(self.included_folders) == 0:
-            self.persistent_notification(f"invalid {APP_NAME} config", 
+            self.p_notification(f"invalid {APP_NAME} config",
             f"`included_folders` parameter in `{APP_CFG_PATH}` should be "
             "a list with at least 1 folder in it", error=True)
         else:
-            for i in range(len(self.included_folders)):
-                self.included_folders[i] = os.path.join(self.included_folders[i], '**/*.yaml')
+            for i, folder in enumerate(self.included_folders):
+                self.included_folders[i] = os.path.join(folder, '**/*.yaml')
             if self.check_lovelace:
                 self.included_folders.append('/config/.storage/**/lovelace*')
 
         self.excluded_folders =  self.args.get('excluded_folders', [])
 
         if not isinstance(self.excluded_folders, list):
-            self.persistent_notification(f"invalid {APP_NAME} config", 
+            self.p_notification(f"invalid {APP_NAME} config",
             f"`excluded_folders` parameter in `{APP_CFG_PATH}` should be "
             "a list not single value", error=True)
         self.report_header = self.args.get('report_header', '=== Watchman Report ===')
@@ -44,25 +44,25 @@ class Watchman(hass.Hass):
 
         self.ignored_items = self.args.get('ignored_items', [])
         if not isinstance(self.ignored_items, list):
-            self.persistent_notification(f"invalid {APP_NAME} config", 
-            f"`ignored_items` parameter in `{APP_CFG_PATH}` should be a list", 
+            self.p_notification(f"invalid {APP_NAME} config",
+            f"`ignored_items` parameter in `{APP_CFG_PATH}` should be a list",
             error=True)
 
         self.ignored_states = self.args.get('ignored_states', [])
         if not isinstance(self.ignored_states, list):
-            self.persistent_notification(f"invalid {APP_NAME} config", 
+            self.p_notification(f"invalid {APP_NAME} config",
             f"`ignored_states` parameter in `{APP_CFG_PATH}` should be a list", error=True)
 
         self.report_path = self.args.get('report_path', '/config/watchman_report.txt')
 
         folder, _ = os.path.split(self.report_path)
         if not os.path.exists(folder):
-            self.persistent_notification("Error from watchman", 
+            self.p_notification("Error from watchman",
             f"Incorrect `report_path` {self.report_path}.", error=True)
 
         if not self.get_flag('.skip_greetings'):
             self.set_flag('.skip_greetings')
-            self.persistent_notification("Hello from watchman!", 
+            self.p_notification("Hello from watchman!",
             "Congratulations, watchman is up and running!\n\n "
             f"Please adjust `{APP_CFG_PATH}` config file according "
             "to your needs or just go to Developer Tools->Events "
@@ -71,36 +71,44 @@ class Watchman(hass.Hass):
         self.listen_event(self.on_event, event=EVENT_NAME)
         #self.audit(ignored_states = self.ignored_states)
 
-    def persistent_notification(self, title, msg, error=False):
+    def p_notification(self, title, msg, error=False):
+        '''Display a persistent notification in Home Assistant'''
         self.call_service("persistent_notification/create", title = title, message = msg )
         if error:
             raise Exception(msg)
 
     def on_event(self, event_name, data, kwargs):
+        '''Process ad.watchman.audit event'''
         create_file = data.get("create_file", True)
         send_notification = data.get("send_notification", True)
-        self.audit(create_report_file = create_file, notification = send_notification, 
+        self.audit(create_report_file = create_file, notification = send_notification,
         ignored_states = self.ignored_states)
 
     def load_services(self):
+        '''Load all available services from HA registry'''
         services = []
         service_data = self.list_services(namespace="global")
-        for s in service_data:
-            services.append(f"{s['domain']}.{s['service']}")
+        for srv in service_data:
+            services.append(f"{srv['domain']}.{srv['service']}")
         return services
 
     def debug(self, msg):
+        '''Debug logging'''
         if self.debug_log:
             self.log(msg)
 
-    def audit(self, create_report_file, notification, ignored_states = []):
+    def audit(self, create_report_file, notification, ignored_states = None):
+        '''Perform audit of entities and services'''
+        if ignored_states is None:
+            ignored_states = []
         start_time = time.time()
-        entity_list, service_list, files_parsed = utils.parse(self.included_folders, 
+        entity_list, service_list, files_parsed = utils.parse(self.included_folders,
         self.excluded_folders, self)
         self.log(f"Found {len(entity_list)} entities, {len(service_list)} services ")
         if files_parsed == 0:
             self.log('No yaml files found, please check apps.yaml config', level="ERROR")
-            self.report('No automation files found, please check apps.yaml config')
+            self.p_notification("Error from watchman!",
+                                "No automation files found, please check apps.yaml config")
             return
 
         entities_missing = {}
@@ -121,14 +129,13 @@ class Watchman(hass.Hass):
             if not state and state != "":
                 state = 'missing'
             if state in ignored_states:
-                continue            
+                continue
             if state in ['missing', 'unknown', 'unavailable']:
                 if entity in excluded_entities:
                     self.debug(f"Ignored entity: {entity}")
                     continue
                 else:
                     entities_missing[entity] = occurences
-        
         for service, occurences in service_list.items():
             if service not in service_registry:
                 if service in excluded_services:
@@ -142,22 +149,22 @@ class Watchman(hass.Hass):
 
         report_chunks = []
 
-        report = "" 
+        report = ""
         report += f"{self.report_header} \n"
 
         if services_missing:
-            report += f"\n=== Missing {len(services_missing)} service(-s) from " 
+            report += f"\n=== Missing {len(services_missing)} service(-s) from "
             report += f"{len(service_list)} found in your config:\n"
-            for service in services_missing: 
+            for service in services_missing:
                 report += f"{service} in {service_list[service]}\n"
                 if len(report) > self.chunk_size:
                     report_chunks.append(report)
                     report = ""
         elif len(service_list) > 0:
-            report += f"\n=== Congratulations, all {len(service_list)} services from " 
+            report += f"\n=== Congratulations, all {len(service_list)} services from "
             report += "your config are available!\n"
         else:
-            report += f"\n=== No services found in configuration files!\n"
+            report += "\n=== No services found in configuration files!\n"
 
         if entities_missing:
             report += f"\n=== Missing {len(entities_missing)} entity(-es) from "
@@ -172,7 +179,7 @@ class Watchman(hass.Hass):
             report += f"\n=== Congratulatiions, all {len(entity_list)} entities from "
             report += "your config are available!\n"
         else:
-            report += f"\n=== No entities found in configuration files!\n"
+            report += "\n=== No entities found in configuration files!\n"
 
         report += f"\n=== Parsed {files_parsed} yaml files in "
         report += f"{(time.time()-start_time):.2f} s. on {time.strftime('%d %b %Y %H:%M:%S')}"
@@ -181,13 +188,13 @@ class Watchman(hass.Hass):
         if create_report_file:
             if not self.get_flag('.skip_achievement'):
                 self.set_flag('.skip_achievement')
-                self.persistent_notification("Achievement unlocked: first report!", 
-                f"Your first report was stored in `{self.report_path}` \n\n " 
+                self.p_notification("Achievement unlocked: first report!",
+                f"Your first report was stored in `{self.report_path}` \n\n "
                 "TIP: set `notify_service` parameter in configuration file to "
                 "receive report via notification service of choice. \n\n "
                 "This is one-time message, it will not bother you in the future.")
 
-            report_file = open(self.report_path, "w")
+            report_file = open(self.report_path, "w", encoding="utf-8")
             for chunk in report_chunks:
                 report_file.write(chunk)
             report_file.close()
@@ -198,21 +205,23 @@ class Watchman(hass.Hass):
                 self.log("Entities and services are fine, no notification required")
 
     def send_notification(self, report):
+        '''Send audit report via a notification service'''
         if not self.notify_service in self.load_services():
-            self.persistent_notification(f"invalid {APP_NAME} config", 
+            self.p_notification(f"invalid {APP_NAME} config",
             f"{self.notify_service} service was not found in service registry. "
             f"Please specify a valid notification service, e.g. `notify.telegram`", error=True)
         elif not self.notify_service:
-            self.persistent_notification(f"invalid {APP_NAME} config", 
+            self.p_notification(f"invalid {APP_NAME} config",
             f"Set `notify_service` parameter in `{APP_CFG_PATH}`, to a notification "
-            "service, e.g. `notify.telegram`", error=True)            
+            "service, e.g. `notify.telegram`", error=True)
         for chunk in report:
             self.call_service(self.notify_service.replace('.','/'), message=chunk)
 
     def set_flag(self, flag):
-        report_file = open(os.path.join(APP_FOLDER, flag), "w")
+        '''Set persistent flag'''
+        report_file = open(os.path.join(APP_FOLDER, flag), "w", encoding="utf-8")
         report_file.close()
 
     def get_flag(self, flag):
+        '''Get persistent flag'''
         return os.path.exists(os.path.join(APP_FOLDER, flag))
-
